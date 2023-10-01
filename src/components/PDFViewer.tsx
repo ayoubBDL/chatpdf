1070f027a953b82cecdc70c8dcc73242b965dc04
ayoubBDL <devfox66@gmail.com>
2023-10-01 08:47:17 +0100

    fix the iframe problem

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -5,7 +5,7 @@ type Props = { pdf_url: string };
 const PDFViewer = ({ pdf_url }: Props) => {
   return (
     <iframe
-      src={`https://docs.google.com/gview?url=${pdf_url}&embedded=true`}
+      src={`//docs.google.com/gview?url=${pdf_url}&embedded=true`}
       className="w-full h-full"
     ></iframe>
   );
